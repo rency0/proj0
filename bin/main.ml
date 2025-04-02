7cf1d020047c2c42a80c07153a84611e879841bc
rency0 <rency.99.w@gmail.com>
2025-04-02 17:22:32 -0400

    terms + types done (kinda)

--- bin/main.ml
+++ bin/main.ml
@@ -7,28 +7,40 @@ type tp =
   | TpBool | TpNat | TpUnit
   | TpArr of tp * tp    (* T1 -> T2 *)
   | TpPair of tp * tp   (* (T1, T2) *)
-  | TpVar of type_name  (* type alpha *)
-  | TpDef of name * tp  (* define "X" = type *)
-  | TpRec of tp         (* maybe ? *)
+  | TpVar of type_name  (* type "X" *)
+  | TpDef of type_name * tp  (* define "X" = type *)
 
 
 type term = 
-  | TmTrue | TmFalse 
   | TmUnit
-  | 
-  | TmIf of term * term * term
-  | TmPair of term * term
-  | TmFst of term
-  | TmSnd of term
-  | TmVar of string
-  | TmLam of string * tp * term
+  | TmVar of var_name
+  | TmLet of var_name * term * term (* let x = t1 in t2 *)
+  (* Bools *)
+  | TmTrue 
+  | TmFalse 
+  | TmIsZero of term
+  (* Nats *)
+  | TmZero 
+  | TmSucc of term 
+  | TmPred of term
+  (* L-calc *)
+  | TmLam of (var_name * tp) * term (*THINK: maybe don't need type in λ (x:T) . t*)
   | TmApp of term * term
+  (* Pairs *)
+  | TmPair of term * term 
+  | TmFst of term 
+  | TmSnd of term
+  (* If then else *)
+  | TmIf of term * term * term
 
 
+
+(*
 type context = (name * tp) list
+*)
 
 
 
-let tp1 = TpArr( TpArr(TpInt, TpInt), TpVar("x"))
+let tp1 = TpArr( TpArr(TpNat, TpNat), TpVar("x"))
 let tp2 = TpDef( "my", tp1) 
 
