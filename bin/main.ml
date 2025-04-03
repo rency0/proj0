ce5639c8b3a03072c98ced4436d31c196d500f6b
rency0 <rency.99.w@gmail.com>
2025-04-03 14:25:49 -0400

    more unify cases

--- bin/main.ml
+++ bin/main.ml
@@ -33,10 +33,41 @@ type term =
   | TmIf of term * term * term
 
 
-
 type context = (var_name * tp) list
 type sigma  = (type_name * tp) list
 type constr = (tp * tp) list 
+
+
+(* Pretty printing functions *)
+let rec tp_to_str (t:tp) : string = match t with 
+  | TpBool -> "Bool"
+  | TpNat -> "Nat"
+  | TpUnit -> "Unit"
+  | TpArr (t1, t2) -> "(" ^ (tp_to_str t1) ^ " -> " ^ (tp_to_str t2) ^ ")"
+  | TpPair (t1, t2) -> "(" ^ (tp_to_str t1) ^ ", " ^ (tp_to_str t2) ^ ")"
+  | TpVar x -> x
+  | TpDef (x, t1) ->  "(" ^ x ^ " := " ^ (tp_to_str t1) ^ ")" 
+let rec term_to_str (t:term) : string = match t with 
+  | TmUnit  -> "unit"
+  | TmTrue  -> "true"
+  | TmFalse -> "false"
+  | TmZero  -> "zero"
+  | TmVar x -> x
+  | TmLet (x, t1, t2) -> "let " ^ x ^ " = " ^ (term_to_str t1) ^ " in " ^ (term_to_str t2)
+  | TmIsZero t1 -> "iszero (" ^ (term_to_str t1) ^ ")"
+  | TmSucc t1 -> "succ (" ^ (term_to_str t1) ^ ")"
+  | TmPred t1 -> "pred (" ^ (term_to_str t1) ^ ")"
+  | TmLam ((x, tp), t) -> "λ" ^ x ^ ":" ^ (tp_to_str tp) ^ "." ^ (term_to_str t)
+  | TmApp (t1, t2) -> "(" ^ (term_to_str t1) ^ ") (" ^ (term_to_str t2) ^ ")"
+  | TmPair (t1, t2) -> "<" ^ (term_to_str t1) ^ ", " ^ (term_to_str t2) ^ ">"
+  | TmFst t1 -> "fst " ^ (term_to_str t1)
+  | TmSnd t1 -> "snd " ^ (term_to_str t1)
+  | TmIf (t1, t2, t3) -> "if (" ^ (term_to_str t1) ^ ") then {" ^ (term_to_str t2) ^ "} else {" ^ (term_to_str t3) ^ "}"
+let print_constraints (constraints:constr) : unit = 
+  List.iter (fun (s, t) -> Printf.printf "%s = %s\n" (tp_to_str s) (tp_to_str t)) constraints
+let print_sigma (sigma:sigma) : unit =
+  List.iter (fun (x, t) -> Printf.printf "%s = %s\n" x (tp_to_str t)) sigma
+(* ------------------------- *)
 
 (* construction of type substitutions satisfying the constraints *)
 let rec unify (constraints:constr) : sigma =
@@ -46,12 +77,17 @@ let rec unify (constraints:constr) : sigma =
       if s = t then unify rest
       else match (s, t) with
         | (TpArr (s1, s2), TpArr (t1, t2)) -> unify ((s1, t1) :: (s2, t2) :: rest)
+        | (TpPair (s1, s2), TpPair (t1, t2)) -> unify ((s1, t1) :: (s2, t2) :: rest)
+        | (TpDef (x, s), t) -> unify ((TpVar x, s) :: (TpVar x , t) :: rest )
+        | (t, TpDef (x, s)) -> unify ((TpVar x, s) :: (TpVar x , t) :: rest )
         | (TpVar x, t) when not (occurs x t) -> (x, t) :: unify (subst (x, t) rest)
         | (t, TpVar x) when not (occurs x t) -> (x, t) :: unify (subst (x, t) rest)
-        | (TpPair (s1, s2), TpPair (t1, t2)) -> unify ((s1, t1) :: (s2, t2) :: rest)
-        (*TODO: *)
-        | _ -> failwith "Unification failed"
-
+        | (TpVar x , TpVar y) -> (
+          if x <> y then failwith "Unification Failed"
+          else (print_endline "Unifying Vars"; unify rest )
+        )
+        | ( _ , _ ) -> failwith "Unification Failed"
+        
   (* X ∈ FV(T) *)
 and occurs (x:type_name) (t:tp) : bool =
   match t with
@@ -75,47 +111,24 @@ and subst ((x:type_name), (t:tp)) (constraints:constr) : constr =
   | _ -> t'
 
 
-
-let rec tp_to_str (t:tp) : string = match t with 
-  | TpBool -> "Bool"
-  | TpNat -> "Nat"
-  | TpUnit -> "Unit"
-  | TpArr (t1, t2) -> "(" ^ (tp_to_str t1) ^ " -> " ^ (tp_to_str t2) ^ ")"
-  | TpPair (t1, t2) -> "(" ^ (tp_to_str t1) ^ ", " ^ (tp_to_str t2) ^ ")"
-  | TpVar x -> x
-  | TpDef (x, t1) ->  "(" ^ x ^ " = " ^ (tp_to_str t1) ^ ")" 
-
-let rec term_to_str (t:term) : string = match t with 
-  | TmUnit  -> "unit"
-  | TmTrue  -> "true"
-  | TmFalse -> "false"
-  | TmZero  -> "zero"
-  | TmVar x -> x
-  | TmLet (x, t1, t2) -> "let " ^ x ^ " = " ^ (term_to_str t1) ^ " in " ^ (term_to_str t2)
-  | TmIsZero t1 -> "iszero (" ^ (term_to_str t1) ^ ")"
-  | TmSucc t1 -> "succ (" ^ (term_to_str t1) ^ ")"
-  | TmPred t1 -> "pred (" ^ (term_to_str t1) ^ ")"
-  | TmLam ((x, tp), t) -> "λ" ^ x ^ ":" ^ (tp_to_str tp) ^ "." ^ (term_to_str t)
-  | TmApp (t1, t2) -> "(" ^ (term_to_str t1) ^ ") (" ^ (term_to_str t2) ^ ")"
-  | TmPair (t1, t2) -> "<" ^ (term_to_str t1) ^ ", " ^ (term_to_str t2) ^ ">"
-  | TmFst t1 -> "fst " ^ (term_to_str t1)
-  | TmSnd t1 -> "snd " ^ (term_to_str t1)
-  | TmIf (t1, t2, t3) -> "if (" ^ (term_to_str t1) ^ ") then {" ^ (term_to_str t2) ^ "} else {" ^ (term_to_str t3) ^ "}"
-(*printing *)
+(* pretty printing functions *)
+  let unify_print (constraints:constr) : unit =
+    print_endline "Constraints:";
+    print_constraints (constraints); 
+    print_endline "\nUnification Result:";
+    print_sigma (unify constraints)
+(* -------------------------- *)
 
 
+(* Testing *)
+let () = unify_print ([
+(* (Bool -> Unit, Y) = ( Y -> Unit, X)  *)  
+(*
+((TpPair(TpArr(TpBool, TpUnit), TpVar "Y")), 
+  ((TpPair(TpArr(TpVar "Y", TpUnit), TpVar "X")))); 
+*) 
+(* Var "A" = Def "B":Nat ; A->C = A->Bool *)
+(TpVar "A", TpDef ("B", TpNat));
+(TpArr (TpVar "A", TpVar "C"), TpArr (TpVar "B", TpBool)); 
 
-let print_constraints (constraints:constr) : unit = 
-  List.iter (fun (s, t) -> Printf.printf "%s = %s\n" (tp_to_str s) (tp_to_str t)) constraints
-
-let print_sigma (sigma:sigma) : unit =
-List.iter (fun (x, t) -> Printf.printf "%s = %s\n" x (tp_to_str t)) sigma
-
-let unify_print (constraints:constr) : unit =
-  print_endline "Constraints:";
-  print_constraints (constraints); 
-  print_endline "\nUnification Result:";
-  print_sigma (unify constraints)
-
-let () = unify_print ([(TpVar "Y", TpPair (TpNat, TpBool)); (TpVar "X", TpArr (TpVar "Y", TpNat))])
-                       
+]) 
